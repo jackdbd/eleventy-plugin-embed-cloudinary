05bad89ae44c677bc6a36835dc077eabaf53c9c7
Giacomo Debidda <jackdebidda@gmail.com>
2021-05-15 00:42:17 +0200

    Use xyz to publish

--- src/.eleventy.ts
+++ src/.eleventy.ts
@@ -4,9 +4,7 @@ import { makeEmbedCloudinary } from './transforms';
 
 export const configFunction = (eleventyConfig: any, userConfig: UserConfig) => {
   const pluginConfig = Object.assign({}, defaultConfig, userConfig);
-  // One can manually use the shortcode...
   eleventyConfig.addShortcode('cloudinaryRespImage', cloudinaryRespImage);
-  // ...or use the transform.
   eleventyConfig.addTransform(
     'embedCloudinary',
     makeEmbedCloudinary(pluginConfig)
