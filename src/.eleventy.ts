8c2a85531ef49448aeca05393a688c67b90e7a3e
Giacomo Debidda <jackdebidda@gmail.com>
2021-05-17 21:38:34 +0200

    Generate docs with TypeDoc and API Extractor

--- src/.eleventy.ts
+++ src/.eleventy.ts
@@ -2,8 +2,16 @@ import { defaultPluginConfig, PluginConfig, UserConfig } from './config';
 import { cloudinaryRespImage } from './shortcodes';
 import { makeEmbedCloudinary } from './transforms';
 
+/**
+ * Configure the plugin to work with your eleventy configuration.
+ *
+ * @public
+ * @param eleventyConfig - Configuration for eleventy.
+ * @param userConfig - Configuration for this plugin.
+ * @remarks
+ * This is the function exported by this plugin.
+ */
 export const configFunction = (eleventyConfig: any, userConfig: UserConfig) => {
-  // plugin config with no missing fields
   const pluginConfig: Required<PluginConfig> = Object.assign(
     {},
     defaultPluginConfig,
